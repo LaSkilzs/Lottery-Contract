c5aca0308bfa8154a6827e5628c066e28f587244
La Fountain <github email address>
2019-06-20 20:21:55 -0400

    set up test environment and changed folder structure

---
+++ Server/test/lottery.test.js
@@ -0,0 +1,13 @@
+const Lottery = artifacts.require("Lottery.sol");
+
+let lottery;
+let account;
+
+// beforeEach(async() => {
+
+// })
+// contract(Lottery, accounts => {
+//   it('should ............', => {
+
+//   })
+// })
